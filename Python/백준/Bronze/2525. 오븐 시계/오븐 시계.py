565a60207c94e40a21db8050b478337c48b6286a
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2024-06-26 17:21:19 +0900

    Update 오븐 시계.py

--- Python/백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ Python/백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,11 +1,28 @@
-h, m = map(int, input().split())
-C = int(input())
+H, M = map(int, input().split())
+timer = int(input()) 
 
-h += C // 60
-m += C % 60
+H += timer // 60
+M += timer % 60
 
-if m>=60:
-    h += 1
-    print(h%24, m-60)
-else:
-    print(h%24, m)
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H,M)
+
+'''
+현재 시간이라고 해서 현재 시간 구해서 작성했는데 런타임 오류 발생
+import datetime as dt
+hour=dt.datetime.now().hour;
+minute=dt.datetime.now().minute;
+print(hour,minute);
+c = int(input().strip())
+
+if c<60:
+    print(hour,minute+c);
+
+elif 60 <= c <= 1000:
+  print(hour+int(c/60),minute+(c%60));
+'''
