8b34e002235c6d07fdc423ed2dbc6e38f6f75d9c
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2024-06-29 13:23:00 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,19 @@
+a,b,c=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+if(a==b==c):
+    print(10000+a*1000)
+elif(a==b):
+    print(1000+a*100)
+elif(b==c):
+    print(1000+b*100)
+elif(c==a):
+    print(1000+c*100)
+else:
+    if(a>b and a>c):
+        print(a*100)
+    elif(b>a and b>c):
+        print(b*100)
+    elif(c>a and c>b):
+        print(c*100)
