5e8187693d7996fa54b90186f185d419c485362a
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2025-04-23 21:48:57 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,11 +1,10 @@
 function solution(n) {
     var answer = Math.sqrt(n);
-    var result = 0;
     if(Number.isInteger(answer)){
-        result=(answer+1)*(answer+1)
+        return (answer+1)**2;
     }
     else{
-        result=-1
+        return -1
     }
-    return result;
+   
 }
