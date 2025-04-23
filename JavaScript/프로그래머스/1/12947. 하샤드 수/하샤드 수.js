5306a9704f0c727f75b2f9e1893538056b9cac5e
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2025-04-23 21:52:35 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ JavaScript/프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,16 +1,16 @@
 function solution(x) {
     var answer = true;
-    let sum=0;
-    var arr=String(x).split('');
+    var xString = String(x);
+    var arr = xString.split('');
+    var sum = 0;
     for(var i=0; i<arr.length; i++){
-        sum+=Number(arr[i]);
+        sum+=Number(arr[i])
     }
-    if(x%sum===0){
+    if(x%sum==0){
         answer=true;
     }
     else{
         answer=false;
     }
-    
     return answer;
 }
